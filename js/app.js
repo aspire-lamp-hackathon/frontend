aa33ef04de5021d11fbf87c68dd2261c60818004
marimuthuvm <nithianandam.karounakarane@aspiresys.com>
2015-10-16 15:50:19 +0530

    UI cleanup

--- js/app.js
+++ js/app.js
@@ -1,3 +1,73 @@
+/* Directives */
+var syd = angular.module('OtdDirectives', []);
+    
+syd.directive('googlePlaces', function(){
+    return {
+        restrict:'E',
+        scope: {location:'=',location2:'='},
+        
+        template: '<label for="google_places_ac1"> Pickup Point :</label><input class="google_places_ac" name="google_places_ac1" type="text" class="input-block-level"/>'+
+                  '<label for="google_places_ac1">Drop Point :</label><input class="google_places_ac" name="google_places_ac2" type="text" class="input-block-level"/>',
+
+        link: function($scope, elm, attrs){
+            var autocomplete = new google.maps.places.Autocomplete($(".google_places_ac")[0], {});
+            google.maps.event.addListener(autocomplete, 'place_changed', function() {
+                var place = autocomplete.getPlace();
+                $scope.location = {'lat':place.geometry.location.lat(), 'lng':place.geometry.location.lng()};
+                $scope.$apply();
+            });
+            
+            var autocomplete2 = new google.maps.places.Autocomplete($(".google_places_ac")[1], {});
+            google.maps.event.addListener(autocomplete2, 'place_changed', function() {
+                var place = autocomplete2.getPlace();
+                $scope.location2 = {'lat':place.geometry.location.lat(), 'lng':place.geometry.location.lng()};
+                $scope.$apply();
+            });
+        }
+    }
+});
+ 
+syd.directive('datetimepicker', function() {
+  return {
+    require: 'ngModel',
+    link: function(scope, el, attr, ngModel) {
+      $(el).datetimepicker({
+        onSelect: function(dateText) {
+          scope.$apply(function() {
+            ngModel.$setViewValue(dateText);
+          });
+        }
+      });
+    }
+  };
+});
+
+/** Controllers **/
+
+syd.controller('LoginCtrl', ['$scope', '$http',
+    function ($scope, $http) {
+		console.log("test")
+	}
+]);
+
+/** End of Controllers **/
+
+/** Routers **/ 
+syd.config(['$routeProvider',
+function($routeProvider) {
+  $routeProvider.
+    when('/login', {
+      controller: 'PhoneListCtrl'
+    }).
+    when('/register', {
+      controller: 'PhoneDetailCtrl'
+    }).
+    otherwise({
+      redirectTo: '/index'
+    });
+}]);
+/** End of Routers **/
+
 var directionsDisplay;
 var directionsService = new google.maps.DirectionsService();
 var map;
@@ -49,75 +119,3 @@ function SearchForm($scope){
 
 
 google.maps.event.addDomListener(window, 'load', initialize); 
-
-/* Directives */
-var syd = angular.module('OtdDirectives', []);
-    
-syd.directive('googlePlaces', function(){
-    return {
-        restrict:'E',
-        scope: {location:'=',location2:'='},
-        
-        template: '<label for="google_places_ac1"> Pickup Point :</label><input class="google_places_ac" name="google_places_ac1" type="text" class="input-block-level"/>'+
-                  '<label for="google_places_ac1">Drop Point :</label><input class="google_places_ac" name="google_places_ac2" type="text" class="input-block-level"/>',
-
-        link: function($scope, elm, attrs){
-            var autocomplete = new google.maps.places.Autocomplete($(".google_places_ac")[0], {});
-            google.maps.event.addListener(autocomplete, 'place_changed', function() {
-                var place = autocomplete.getPlace();
-                $scope.location = {'lat':place.geometry.location.lat(), 'lng':place.geometry.location.lng()};
-                $scope.$apply();
-            });
-            
-            var autocomplete2 = new google.maps.places.Autocomplete($(".google_places_ac")[1], {});
-            google.maps.event.addListener(autocomplete2, 'place_changed', function() {
-                var place = autocomplete2.getPlace();
-                $scope.location2 = {'lat':place.geometry.location.lat(), 'lng':place.geometry.location.lng()};
-                $scope.$apply();
-            });
-        }
-    }
-});
- 
-syd.directive('datetimepicker', function() {
-  return {
-    require: 'ngModel',
-    link: function(scope, el, attr, ngModel) {
-      $(el).datetimepicker({
-        onSelect: function(dateText) {
-          scope.$apply(function() {
-            ngModel.$setViewValue(dateText);
-          });
-        }
-      });
-    }
-  };
-});
-
-/** Controllers **/
-
-syd.controller('LoginCtrl', ['$scope', '$http',
-    function ($scope, $http) {
-
-		
-
-	}
-]);
-
-/** End of Controllers **/
-
-/** Routers **/ 
-syd.config(['$routeProvider',
-function($routeProvider) {
-  $routeProvider.
-    when('/login', {
-      controller: 'PhoneListCtrl'
-    }).
-    when('/register', {
-      controller: 'PhoneDetailCtrl'
-    }).
-    otherwise({
-      redirectTo: '/index'
-    });
-}]);
-/** End of Routers **/
